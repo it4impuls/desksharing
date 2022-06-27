b211c51962657c414f62ef819a001ab80996335d
marilynias <98028585+marilynias@users.noreply.github.com>
2022-06-27 12:23:18 +0200

    (hopefully) fixed path issues in bat

--- setup.bat
+++ setup.bat
@@ -1,22 +1,16 @@
 @ECHO OFF
-IF EXIST venv\ (
-GOTO runner
-)ELSE (
-GOTO installer
-)
+IF EXIST venv\ GOTO runner
+ELSE GOTO installer
 
-:runner
-ECHO removing old installation...
-@RD /S /Q %~dp0venv
 
 :installer
 ECHO instaling enviroment...
-START /B /WAIT py -m venv venv
+START /B /WAIT py -m venv %~dp0venv
 ECHO installing libraries...
 START /B /WAIT %~dp0venv\Scripts\python.exe -m pip install -r %~dp0req.txt
-GOTO starter
+GOTO runner
 
-:starter
+:runner
 ECHO starting programm...
 START /B /WAIT %~dp0venv\Scripts\python.exe %~dp0view.py
 GOTO end
