cc649afb7edb6db36181331585d9e971aeac8fbc
marilynias <98028585+marilynias@users.noreply.github.com>
2022-09-26 12:43:14 +0200

    Make Folder-/filenames variable

--- reinstall.bat
+++ reinstall.bat
@@ -1,5 +1,11 @@
 @ECHO OFF
-IF EXIST venv\ (
+SET env_foldername=venv
+SET modulefile=req.txt
+SET runfile=view.py
+
+SET root=%~dp0
+
+IF EXIST %env_foldername%\ (
 GOTO runner
 )ELSE (
 GOTO installer
@@ -7,18 +13,18 @@ GOTO installer
 
 :runner
 ECHO removing old installation...
-@RD /S /Q %~dp0venv
+@RD /S /Q %root%%env_foldername%
 
 :installer
-ECHO instaling enviroment...
-START /B /WAIT py -m venv venv
+ECHO instaling enviroment (this might take a few minutes)...
+START /B /WAIT py -m venv %env_foldername%
 ECHO installing libraries...
-START /B /WAIT %~dp0venv\Scripts\python.exe -m pip install -r %~dp0req.txt
+IF EXIST %root%%modulefile% START /B /WAIT %root%%env_foldername%\Scripts\python.exe -m pip install -r %root%%modulefile%
 GOTO starter
 
 :starter
 ECHO starting programm...
-START /B /WAIT %~dp0venv\Scripts\python.exe %~dp0view.py
+START /B /WAIT %root%%env_foldername%\Scripts\python.exe %root%%runfile%
 GOTO end
 
 :end
